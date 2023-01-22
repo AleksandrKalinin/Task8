7559baa3b012365f6ceffb8c8c7c4e45fb173154
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2023-01-22 21:33:41 +0300

    Added basic markup and functionality in calendar component

--- src/components/CalendarCarousel.vue
+++ src/components/CalendarCarousel.vue
@@ -1,90 +1,29 @@
 <template>
   <div class="calendar">
     <div class="calendar__wrapper">
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Monday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Tuesday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Wednesday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item calendar-item_selected">
-        <span class="calendar-item__day">Thursday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Friday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Saturday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Sunday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Monday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Tuesday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Wednesday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item calendar-item_selected">
-        <span class="calendar-item__day">Thursday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Friday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Saturday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Sunday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Monday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Tuesday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Wednesday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item calendar-item_selected">
-        <span class="calendar-item__day">Thursday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Friday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Saturday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
-      <div class="calendar__item calendar-item">
-        <span class="calendar-item__day">Sunday</span>
-        <span class="calendar-item__date">8</span>
-      </div>
+      <template v-if="days.length !== 0">
+        <div
+          class="calendar__item calendar-item"
+          v-for="(item, index) in days"
+          v-bind:key="index"
+          v-bind:class="{
+            'calendar-item_selected': item.getDate() === currentDate.getDate(),
+          }"
+        >
+          <span class="calendar-item__day">{{ names[item.getDay()] }}</span>
+          <span class="calendar-item__date"
+            >{{ months[item.getMonth()] }} {{ item.getDate() }},
+            {{ item.getFullYear() }}</span
+          >
+          <div class="calendar-item__tasks calendar-tasks">
+            <span class="calendar-tasks__item calendar-task_completed"></span>
+            <span class="calendar-tasks__item calendar-task_pending"></span>
+          </div>
+        </div>
+      </template>
+      <template v-else>
+        <div>Loading...</div>
+      </template>
     </div>
   </div>
 </template>
@@ -92,6 +31,51 @@
 <script>
 export default {
   name: "CalendarCarousel",
+  data() {
+    return {
+      currentDate: new Date(),
+      days: [],
+      names: [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ],
+      months: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+    };
+  },
+  methods: {
+    renderItems: function () {
+      const year = this.currentDate.getFullYear();
+      const month = this.currentDate.getMonth();
+      const days = [];
+      const startDate = new Date(year, month, 1);
+      while (startDate.getMonth() === month) {
+        days.push(new Date(startDate));
+        startDate.setDate(startDate.getDate() + 1);
+      }
+      this.days = days;
+    },
+  },
+  mounted() {
+    this.renderItems();
+  },
 };
 </script>
 
@@ -123,16 +107,35 @@ export default {
       cursor: pointer
 
       .calendar-item__day
-        font-size: 16px
-        font-weight: 700
+        font-size: 18px
+        text-transform: uppercase
+        letter-spacing: 1px
+        font-weight: 600
+        margin-bottom: 12px
 
       .calendar-item__date
-        font-size: 22px
-        font-weight: 700
+        font-size: 14px
+
+      .calendar-item__tasks
+        margin-top: 10px
+        display: flex
+        justify-content: center
+
+        .calendar-tasks__item
+          width: 10px
+          height: 10px
+          border-radius: 50%
+          margin: 0 3px
+
+        .calendar-task_completed
+          background: #BBBBBB
+
+        .calendar-task_pending
+          background: #222222
 
     .calendar-item_selected
-      color: #ffffff
-      background: tomato
+      color: #222222
+      border: 2px solid #222222
 
   .calendar__wrapper::-webkit-scrollbar
     display: none
