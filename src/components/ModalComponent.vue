34307ab0abe869243990299d34b64a2c16ed7a31
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2023-02-01 14:51:35 +0300

    Separate files for icons

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -2,12 +2,7 @@
   <div class="overlay">
     <div class="modal">
       <span class="icon" v-on:click="toggleModal">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-          <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-          <path
-            d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"
-          />
-        </svg>
+        <img v-bind:src="require('@/assets/close.svg')" />
       </span>
       <form class="modal__form modal-form">
         <textarea class="modal-form__text" v-model="text">
